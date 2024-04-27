e5f58d2d9d8b158e30d7702d29ea5d9a4a424946
Leonid Demidov <l.demidov@maximaster.ru>
2024-04-27 13:10:23 +0300

    Пакет для свг, начало детальной

--- resources/js/Pages/Detail.tsx
+++ resources/js/Pages/Detail.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import MainLayout from "@/Layouts/MainLayout";
+import {Link} from "@inertiajs/react";
+// @ts-ignore
+import Arrow from '../../../public/img/left-arrow.svg?react';
+import '../../less/common.blocks/detail/detail.less';
 
 export default class Detail extends React.Component {
     render() {
@@ -8,7 +12,14 @@ export default class Detail extends React.Component {
                 <MainLayout>
                     <div className={'container'}>
                         <div className={'container__sections-left'}>
-
+                            <div className={'detail'}>
+                                <div className={'detail__header'}>
+                                    <Link className={'detail__back'} href={route('main')}>
+                                        <Arrow/>
+                                        <span>Назад</span>
+                                    </Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </MainLayout>
