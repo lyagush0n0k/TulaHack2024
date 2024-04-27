4fc3c99057c09568f4b696fcde68c69fd1b3d3d4
Игорь Шевченко <i.shevchenko@maximaster.ru>
2024-04-27 18:00:39 +0300

    Контроллер для пользователей в админке

---
+++ app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class UsersController extends Controller
+{
+    public function index(Request $request)
+    {
+        $search = $request->get('search');
+        $users = User::search($search)->paginate(25);
+
+        return Inertia::render('Admin/Users/Users', ['users' => $users]);
+    }
+}
