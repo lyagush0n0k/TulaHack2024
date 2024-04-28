86b6488890969dcf14735a4dd90bb5635d1df9f0
Leonid Demidov <l.demidov@maximaster.ru>
2024-04-28 10:51:36 +0300

    Правки детальной

--- resources/js/Pages/Detail.tsx
+++ resources/js/Pages/Detail.tsx
@@ -81,18 +81,7 @@ export default function Detail({auth, restaurant, schedule, bookings, media}: Pa
         {value: '10', label: '10'},
     ];
 
-    const tableOptions = [
-        {value: '1', label: '1'},
-        {value: '2', label: '2'},
-        {value: '3', label: '3'},
-        {value: '4', label: '4'},
-        {value: '5', label: '5'},
-        {value: '6', label: '6'},
-        {value: '7', label: '7'},
-        {value: '8', label: '8'},
-        {value: '9', label: '9'},
-        {value: '10', label: '10'},
-    ];
+    var tableOptions = [];
 
     const fetchAvailableTables = async (e) => {
         e.preventDefault();
@@ -115,6 +104,10 @@ export default function Detail({auth, restaurant, schedule, bookings, media}: Pa
         }
 
         document.querySelector('.detail__container--booking').style.display = 'flex';
+
+        responseData.forEach((element) => {
+            tableOptions.push({value: element.id, label: element.number});
+        });
     };
 
     return (
