5328bef1e655c688776128ad371d06749eb71346
Leonid Demidov <l.demidov@maximaster.ru>
2024-04-28 12:17:23 +0300

    Кнопка на главную

--- resources/js/Components/Header.tsx
+++ resources/js/Components/Header.tsx
@@ -42,8 +42,8 @@ export default function Header({ auth }: { auth: any }) {
               <nav className={'header__nav nav'}>
                 <ul className="nav__list">
                   <li className="nav__list-item">
-                    <a className={'active'} href="javascript:void(0)">
-                      Home
+                    <a className={'active'} href={route('main')}>
+                      Главная
                     </a>
                   </li>
                   <li className="nav__list-item">
@@ -97,8 +97,8 @@ export default function Header({ auth }: { auth: any }) {
                                   <div className="menu__body">
                                       <ul className="nav__list">
                                           <li className="nav__list-item">
-                                              <a className={'active'} href="javascript:void(0)">
-                                                  Home
+                                              <a className={'active'} href={route('main')}>
+                                                  Главная
                                               </a>
                                           </li>
                                           <li className="nav__list-item">
